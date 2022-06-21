0b602a468fe76fdcbe4634a1501679cde27fc85d
Karenkoech <karenkoech3@gmail.com>
2022-06-21 02:58:18 -0400

    have forms

---
+++ neighbour/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import NeighbourHood, Business, Post
+
+
+class NeighbourHoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        exclude = ('admin',)
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighbourhood')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
